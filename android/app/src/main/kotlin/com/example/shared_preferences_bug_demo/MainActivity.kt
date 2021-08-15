e5dc22ade3bf44d7951f901017dbe75cf855b7ac
Cobinja <cobinja@yahoo.de>
2021-08-15 21:51:58 +0200

    Initial commit

---
+++ android/app/src/main/kotlin/com/example/shared_preferences_bug_demo/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.shared_preferences_bug_demo
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,119 @@
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+
+const String _PREF_KEY = 'my_key';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Shared Preferences Bug Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Shared Preferences Bug Demo'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  
+  String? _value;
+  
+  _MyHomePageState() {
+    _readValue();
+  }
+  
+  void _readValue() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    setState(() {
+      _value = prefs.getString(_PREF_KEY);
+    });
+  }
+  
+  void _setValueWithExceptionHandling() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    
+    try {
+      // Trying to set a string that starts with the list identifier.
+      // This throws a Platform Exception
+      await prefs.setString(_PREF_KEY, 'VGhpcyBpcyB0aGUgcHJlZml4IGZvciBhIGxpc3Qu42.0');
+    }
+    catch (e) {
+      debugPrint('Exception caught: ' + e.runtimeType.toString());
+    }
+    
+    _readValue(); // This will read the value 'VGhpcyBpcyB0aGUgcHJlZml4IGZvciBhIGxpc3Qu42.0' from the preference cache even though it is not stored on the Android side
+  }
+  
+  void _setValueWithoutExceptionHandling() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    
+    // Trying to set a string that starts with the list identifier.
+    // This throws a Platform Exception
+    await prefs.setString(_PREF_KEY, 'VGhpcyBpcyB0aGUgcHJlZml4IGZvciBhIGxpc3Qu42.0');
+    
+    _readValue(); // This will read the value 'VGhpcyBpcyB0aGUgcHJlZml4IGZvciBhIGxpc3Qu42.0' from the preference cache even though it is not stored on the Android side
+  }
+  
+  void _clearAll() async {
+    SharedPreferences prefs = await SharedPreferences.getInstance();
+    await prefs.clear();
+    setState(() {
+      _value = null;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Text(
+              'This demonstrates a bug in shared_preferences\nwhere values are cached,\nbut not stored on Android.\n',
+              style: Theme.of(context).textTheme.subtitle1,
+            ),
+            Text(
+              'This is the value taken from preference cache:\n',
+            ),
+            Text(
+              (_value ?? 'No value found in the cache') + '\n',
+              style: Theme.of(context).textTheme.headline6,
+            ),
+            ElevatedButton(
+              onPressed: _setValueWithExceptionHandling,
+              child: Text('Set value with exception handling')
+            ),
+            ElevatedButton(
+              onPressed: _setValueWithoutExceptionHandling,
+              child: Text('Set value without exception handling')
+            ),
+            ElevatedButton(
+              onPressed: _clearAll,
+              child: Text('Clear Cache and platform-side storage')
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
